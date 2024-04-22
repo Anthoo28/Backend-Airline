7bad112ca3e180bf5b5823d986f35b017367b416
Cesar Andia Ascama <zarek1404@gmail.com>
2024-04-22 11:46:17 -0500

    first commit

---
+++ src/test/java/backend/airline/AirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.airline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
